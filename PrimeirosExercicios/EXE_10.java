bb6f4be3675b8cabc193bacdf2f5b6a4d557882c
Luis Henrique Maicá de Souza <lhms2409@gmail.com>
2025-08-27 20:35:37 -0300

    27/08/2025

--- PrimeirosExercicios/EXE_10.java
+++ PrimeirosExercicios/EXE_10.java
@@ -1,13 +1,25 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+
+/* 10. Salário do Cupincha: O Cupincha, mascote do nosso time, precisa calcular 
+seu novo salário! Ele ganha R$ 25 por hora e trabalha 8 horas por dia. Crie um programa
+que pergunte quantos dias ele trabalhou e calcule seu novo salário.
  */
 package PrimeirosExercicios;
 
-/**
- *
- * @author LUISHENRIQUEMAICADES
- */
+import java.util.Scanner;
+
 public class EXE_10 {
-    
+
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Quantos dias nosso mascote cupincha trabalhou?");
+        int dias = teclado.nextInt();
+
+        double hora = 25;
+        
+        double novoSalario = dias * hora;
+        
+        System.out.println("O salario do cupincha de "+dias+" dias é de: R$:"+novoSalario);
+        
+    }
 }
---
+++ PrimeirosExercicios/EXE_11.java
@@ -0,0 +1,16 @@
+
+/*11. Multa por Excesso de Velocidade: Em uma rodovia, a velocidade máxima 
+permitida é de 80 km/h. Crie um programa que pergunte a velocidade de um carro
+e calcule a multa, caso o motorista tenha excedido o limite. A multa é de R$ 5 por
+cada km/h acima do limite. Exiba uma mensagem informando se o motorista foi multado e, em 
+caso afirmativo, qual o valor da multa.
+*/
+
+package PrimeirosExercicios;
+
+public class EXE_11 {
+    public static void main(String[] args) {
+        
+        
+    }
+}
